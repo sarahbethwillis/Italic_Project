c13c012533a258a872c862de598b6c975143df14
Sarah Willis <sarahbethwillis@gmail.com>
2019-01-05 10:37:01 -0500

    Deleted node libs, changed display of products

--- scroll.js
+++ scroll.js
@@ -15,34 +15,10 @@ function myFunction() {
 
 
 
-function elementFromTop(elemTrigger, elemTarget, classToAdd, distanceFromTop, unit) {
-    var winY = window.innerHeight || document.documentElement.clientHeight,
-        elTriggerLength = elemTrigger.length,
-        elTargetLength, distTop, distPercent, distPixels, distUnit, elTarget, i, j;
-    for (i = 0; i < elTriggerLength; ++i) {
-        elTarget = document.querySelectorAll('.'+elemTarget);
-        elTargetLength = elTarget.length;
-        distTop = elemTrigger[i].getBoundingClientRect().top;
-        distPercent = Math.round((distTop / winY) * 100);
-        distPixels = Math.round(distTop);
-        distUnit = unit == 'percent' ? distPercent : distPixels;
-        if (distUnit <= distanceFromTop) {
-            if (!hasClass(elemTrigger[i], elemTarget)) {
-               for (j = 0; j < elTargetLength; ++j) {
-                   if (!hasClass(elTarget[j], classToAdd)) { addClass(elTarget[j], classToAdd); }
-                   }
-               } else {
-               if (!hasClass(elemTrigger[i], classToAdd)) { addClass(elemTrigger[i], classToAdd); }                }
-           } else {
-           delClass(elemTrigger[i], classToAdd);
-           if (!hasClass(elemTrigger[i], elemTarget)) {
-              for (j = 0; j < elTargetLength; ++j) { delClass(elTarget[j], classToAdd); }
-               }
-           }
-       }
-   }
-// params:  trigger element, target element class, classes to add to target element, trigger element distance from top, unit ('percent' or 'pixels')
-// usage:   elementFromTop(elemTrigger, elemTarget, classToAdd, distanceFromTop, unit);
+
+
+
+
 
 
 
@@ -75,13 +51,6 @@ var animateHTML = function() {
   };
 };
 animateHTML().init();
-
-
-
-AOS.init({
-  duration: 1200,
-})
-
 
 
 
